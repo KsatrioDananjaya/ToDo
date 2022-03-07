9175aa8514c838da1baa31849a12f78cc66c2e46
Ksatrio <ksatrio.dana.ft20@mail.umy.ac.id>
2022-03-07 23:28:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String nama, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.btSignin);
+        edemail=findViewById(R.id.edEmail);
+        edpassword=findViewById(R.id.edPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                Toast.makeText(MainActivity.this,"email anda: "+nama+" dan password anda: "+password+"", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
